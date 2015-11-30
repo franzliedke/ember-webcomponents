d5791f2425fa8172e2511142e1b2c37f072e0d07
Franz Liedke <franz@develophp.org>
2015-11-30 17:08:02 +0100

    Fix installation procedure

--- blueprints/ember-webcomponents/index.js
+++ blueprints/ember-webcomponents/index.js
@@ -1,6 +1,11 @@
 module.exports = {
   description: 'Prepare your application for webcomponents',
 
+  normalizeEntityName: function() {
+    // NOOP
+    // This prevents an error about missing entity names
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('webcomponents/webcomponentsjs');
   }
